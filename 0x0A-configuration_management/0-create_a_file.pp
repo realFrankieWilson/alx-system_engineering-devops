fd73cf12dabc9ce5fca5f325005d8e56150295e8
realFrankieWilson <frankuwill101@gmail.com>
2023-11-25 02:08:40 +0100

    hot fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file {'school':
-    path     => '/tmp/school',
-    mode     => '0744',
-    owner    => 'www-data',
-    group    => 'www-data',
+# Create a file using puppet
+file { '/tmp/school':
+    ensure  => 'file',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet',
 
 }
