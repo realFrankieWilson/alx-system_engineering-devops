0c7813c86cd5662f75b55d9effb2064b490576b6
realFrankieWilson <frankuwill101@gmail.com>
2024-01-19 10:24:19 +0100

    ore fixs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+A module that contains a method that returns a Reddit api
+of the total number of subscribers
+"""
+
 import requests
 
 
@@ -6,13 +11,14 @@ def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given given subreddit
     """
+    user_agent = 'User Agent'
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-            'User-Agent': 'Agent v2'
-            }
+    
+    headers = {'User-Agent': user_agent}
+
     resp = requests.get(url, headers=headers, allow_redirects=False)
-    # json_obj = sub.json()
-    if resp.status_code == 404:
+    
+    if resp.status_code != 200:
         return 0
     data = resp.json().get('data')
     return data.get('subscribers')
