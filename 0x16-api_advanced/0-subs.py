5dbc48f6219a28ebf672b61f97a00615ae7fc9aa
realFrankieWilson <frankuwill101@gmail.com>
2024-01-19 10:04:39 +0100

    hot fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,13 @@ def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given given subreddit
     """
-    api_ = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    subscribe = requests.get(
-            api_, allow_redirects=False, headers={
-                'User-Agent': 'My User Agent 2.0'
-                }
-            )
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+            'User-Agent': 'Agent v2'
+            }
+    resp = requests.get(url, headers=headers, allow_redirects=False)
     # json_obj = sub.json()
-    if subscribe.status_code == 404:
+    if resp.status_code == 404:
         return 0
-    return (subscribe.json().get('data').get('subscribers'))
+    data = resp.json().get('data')
+    return data.get('subscribers')
