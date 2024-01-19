9fac853e92e821619bdbbac9cc339e041e574acf
realFrankieWilson <frankuwill101@gmail.com>
2024-01-19 14:14:59 +0100

    hot fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
+"""
+A module that contains a method that returns a Reddit api
+of the subscribers activities
+"""
+
 import requests
 
 
 def top_ten(subreddit):
     """
-    Returns the the titles of the top ten posts of a given subreddit
+    prints the titles of the 10 hottest posts of a given subreddit
     """
-    api_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    subscribe = requests.get(
-            api_url, allow_redirects=False, headers={
-                'User-Agent': 'My User Agent 2.0'
-                }
-            )
-    if subscribe.status_code == 404:
-        print("None")
-        return
-    json_obj = subscribe.json().get("data")
-    [print(info.get("data").get("title")) for info in json_obj.get("children")]
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 2.0'}
+    sub_obj = requests.get(url, allow_redirects=False, headers=headers)
+
+    if sub_obj:
+        obj_file = sub_obj.json()
+        res_data = obj_file.get('data').get('children')
+
+        data_lists = []
+
+        for i in res_data[:10]:
+            data_lists.append(i.get('data').get('title'))
+
+        for j in data_lists:
+            print(j)
+    else:
+        print('None')
