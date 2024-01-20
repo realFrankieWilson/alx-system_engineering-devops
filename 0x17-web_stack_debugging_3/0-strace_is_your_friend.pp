a696719cd3a0c796e7c4873b5fcf83892f20c90f
frankiewilson <frankuwill101@gmail.com>
2024-01-20 17:44:55 +0000

    hot fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # An automated way of fixing a 500 error code returned by apache server.
-file { 'fixedType':
-  command  => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-local.php',
+exec { 'fixedType':
+  command  => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
   provider => shell,
 }
