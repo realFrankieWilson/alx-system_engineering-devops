7cc32a247313aa236094da0e0e8e708f6040d8d7
realFrankieWilson <frankuwill101@gmail.com>
2024-03-04 11:42:49 +0100

    made some modification to file 0-sub.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """
-A module that contains a method that returns a Reddit api
-of the total number of subscribers
+    A module that contains a method that returns a Reddit api
+    of the total number of subscribers
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Returns the number of subscribers for a given given subreddit
+        Returns the number of subscribers for a given given subreddit
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-
-    headers = {'User-Agent': 'User-Agent 2.0'}
-
-    resp = requests.get(url.format(subreddit), headers, allow_redirects=False)
-
-    if resp:
-        data = resp.json().get('data').get('subscribers')
-        return data
-    return 0
+    header = requests.get(
+        url, allow_redirects=False, headers={
+            'User-Agent': 'My machin 1.0'
+        }
+        )
+    resp = header.json()
+    if header:
+        return (resp.get('data').get('subscribers'))
+    return (0)
