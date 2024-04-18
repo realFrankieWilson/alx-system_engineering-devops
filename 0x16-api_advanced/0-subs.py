c437e4eb0957e9b2becf02d2d3405df3df4beb78
realFrankieWilson <frankuwill101@gmail.com>
2024-04-18 08:44:12 +0100

    updated the sys-dev file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-""" querries that retuens Reddit api, the total nos of subscribers"""
+""" A script that queries a reddit api and returns the number of subscribers """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """a function that queries the Reddit API and returns
-    the number of subscribers(not active users, total subscribers)
-    for a given subreddit. If an invalid subreddit is given,
-    the function should return 0.
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+  """ A function that returns the number of a reddit subscribers. """
+  url = "http://www.reddit.com/r/{}/about.json"
+  headers = {"User-Agent": "YourBot/1.0 by Frankiewilson"}
+  resp = requests.get(url.format(subreddit), headers=headers, allow_redirects=False)
 
-    headers = {
-            "User-Agent": "Google chrome version 115.0.5790.171"
-            }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    result = response.json().get("data")
-    return result.get("subscribers")
+  if resp.status_code == 200:
+    sub = resp.json().get("data").get("subscribers")
+    return sub
+  else:
+    return 0
